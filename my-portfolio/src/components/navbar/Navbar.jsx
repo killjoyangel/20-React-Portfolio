1b9e91babc77165273373d8308eaa7d76076a90c
Ariel Marchand <marchand.aa@gmail.com>
2021-08-04 20:47:32 -0600

    updating navbar

---
+++ my-portfolio/src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.scss"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">blah blah</a>
+                </div>
+                <div className="right"></div>
+            </div>
+        </div>
+    )
+}
