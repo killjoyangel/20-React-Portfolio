a654cdb2903dfea673ffe502502fe45a9408f903
Ariel Marchand <marchand.aa@gmail.com>
2021-08-04 20:05:33 -0600

    pulled over previous changes

---
+++ my-portfolio/src/componnents/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./navbar.scss"
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="left">
+                    <div className="right"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
